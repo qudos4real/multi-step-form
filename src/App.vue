d15fb2795436e875caefc9875e9baaafc995ee55
Qudos4real <qudos4real@yahoo.com>
2022-12-31 14:55:06 +0100

    Initialized and setting up first page

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,50 @@
 <template>
-  <nav>
+  <!-- <nav>
     <router-link to="/">Home</router-link> |
     <router-link to="/about">About</router-link>
-  </nav>
+  </nav> -->
+  <div class="main-wrapper">
+    <div class="bk-img">
+      <nav>    
+          <div v-for="item in sidebarData" :key="item.step">
+            <h3 class="step">{{item.step}}</h3>
+            <p class="title">{{item.title}}</p>
+          </div>       
+      </nav>
+     
+    </div>
+  </div>
+
   <router-view/>
 </template>
+
+<script>
+export default {
+  // name: 'App',
+  data () {
+    return {
+      sidebarData: [
+        {
+          step: 'Step 1',
+          title: 'Your Info',
+        },
+        {
+          step: 'Step 2',
+          title: 'Select Plan',
+        },
+        {
+          step: 'Step 3',
+          title: 'Add-ons',
+        },
+        {
+          step: 'Step 4',
+          title: 'Summary',
+        },
+      ]
+    }
+  }
+}
+</script>
 
 <style>
 #app {
@@ -14,6 +54,27 @@
   text-align: center;
   color: #2c3e50;
 }
+
+.main-wrapper{
+  width: 1440px;
+  height: 600px;
+}
+
+.bk-img{
+  background-image: url("./assets/images/bg-sidebar-desktop.svg");
+  background-repeat: no-repeat;
+  background-size: auto;
+  height: 568px;
+  width: 40%;
+
+}
+
+
+
+
+
+
+
 
 nav {
   padding: 30px;
---
+++ src/components/PersonalInfo.vue
@@ -0,0 +1,14 @@
+<template>
+    <h2>Personal info </h2>
+  <h3>Please provide your name, email address, and phone number. </h3>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
